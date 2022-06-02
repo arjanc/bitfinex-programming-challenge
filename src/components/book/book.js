f031390f070206972b878509cf8e251f91bbf95f
Arjan Ceelen <arjan@celar.nl>
2022-06-02 13:48:23 +0200

    Ui improvements with fixed decimals

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -8,17 +8,18 @@ const Book = ({ orders }) => {
             <div className="bookHeader">
                 <div style={{width: '40px', minWidth: '40px', maxWidth: '40px'}}>Count</div>
                 <div style={{minWidth: '25%', textAlign: 'right'}}>Amount</div>
-                <div style={{width: '65px', minWidth: '65px', maxWidth: '65px',textAlign: 'right' }}>Total</div>
+                <div style={{width: '65px', minWidth: '65px', maxWidth: '65px', textAlign: 'right' }}>Total</div>
                 <div style={{minWidth: '25%', textAlign: 'right'}}>Price</div>
             </div>
             <div className="bookBody">
                 {orders.map((order, index) => {
                     if (order) {
+                        const total = order.count * order.amount;
                         return (
                             <div className="bookRow" key={`order-${index}`}>
                                 <div style={{width: '40px', minWidth: '40px', maxWidth: '40px'}}>{order.count}</div>
-                                <div style={{minWidth: '25%', textAlign: 'right'}}>{order.amount}</div>
-                                <div style={{width: '65px', minWidth: '65px', maxWidth: '65px',textAlign: 'right' }}>{order.price * order.amount}</div>
+                                <div style={{minWidth: '25%', textAlign: 'right'}}>{order.amount ? order.amount.toFixed(4) : ''}</div>
+                                <div style={{width: '65px', minWidth: '65px', maxWidth: '65px', textAlign: 'right' }}>{total ? total.toFixed(4) : ''}</div>
                                 <div style={{minWidth: '25%', textAlign: 'right'}}>{order.price}</div>
                             </div>
                         )
